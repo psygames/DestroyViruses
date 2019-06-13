14efe34a0e0babf347e343199ce374eb47e65a58
yinlong <799329256@qq.com>
2019-06-13 19:21:15 +0800

    开发同步 2019年06月13日19:21:12

---
+++ DestroyViruses/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using QFramework;
+using UnityEngine;
+
+namespace DestroyViruses
+{
+    public class GameManager : MonoSingleton<GameManager>, IManager
+    {
+        private void Start()
+        {
+            Init();
+        }
+        
+        public void Init()
+        {
+            GameStart();
+        }
+
+        public void RegisterEvent<T>(T msgId, OnEvent process) where T : IConvertible
+        {
+        }
+
+        public void SendEvent<T>(T eventId) where T : IConvertible
+        {
+        }
+
+        public void SendMsg(QMsg msg)
+        {
+        }
+
+        public void UnRegistEvent<T>(T msgEvent, OnEvent process) where T : IConvertible
+        {
+        }
+
+        private void GameStart()
+        {
+            UIMgr.OpenPanel<MainPanel>(prefabName: "Resources/UI/MainPanel");
+        }
+    }
+}
