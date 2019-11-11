4ea362067cd1bc35ffbaf9748c9d7dc8cdb7b9f5
yinlong <799329256@qq.com>
2019-11-11 11:04:57 +0800

    build asset bundle

---
+++ DestroyViruses/Assets/Plugins/Basic/EditorUtil/Editor/ClearMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace ClearTools
+{
+    public static class ClearMenu
+    {
+        private const string clearPersistent = "Tools/清除 PersistentData";
+
+        [MenuItem(clearPersistent)]
+        private static void ClearPersistent()
+        {
+            if (Directory.Exists(Application.persistentDataPath))
+            {
+                Directory.Delete(Application.persistentDataPath, true);
+            }
+        }
+    }
+}
