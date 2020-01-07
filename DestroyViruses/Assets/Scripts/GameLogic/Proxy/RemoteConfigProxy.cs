06423ac011ada9ae695bcb5d6540941a2ed04ec1
yinlong <799329256@qq.com>
2020-01-07 23:26:51 +0800

    remote config ,last google-services

---
+++ DestroyViruses/Assets/Scripts/GameLogic/Proxy/RemoteConfigProxy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DestroyViruses
+{
+    public class RemoteConfigProxy : ProxyBase<RemoteConfigProxy>
+    {
+        protected override void OnInit()
+        {
+            base.OnInit();
+            var defaults = new Dictionary<string, object>();
+            defaults.Add("const_group", "default");
+            Firebase.RemoteConfig.FirebaseRemoteConfig.SetDefaults(defaults);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        public void FetchAsync()
+        {
+        }
+    }
+}
